3e9d46910f2b203b5182167a289ff805de613fc3
Naupaka Zimmerman <naupaka@gmail.com>
2014-12-29 15:25:41 -0700

    Add in code for liquid flowmeter

--- combined_lux_barometer/combined_lux_barometer.ino
+++ combined_lux_barometer/combined_lux_barometer.ino
@@ -1,5 +1,6 @@
 // Output format is YYYYMMDD, 24HRTIME, Barometric_pressure_in_pascals, 
-// temp_from_MPL3115A2_in_deg_C, light_from_TSL2561_in_lux, humidity_DHT_sensor_pcnt, DHT_temp_in_deg_C  
+// temp_from_MPL3115A2_in_deg_C, light_from_TSL2561_in_lux, humidity_DHT_sensor_pcnt, DHT_temp_in_deg_C,
+// Liquid_flow_frequency, liquid_flow_pulses_cumulative_count, liquid_flow_cumulative_liters 
 
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
@@ -40,6 +41,48 @@ DHT dht(DHTPIN, DHTTYPE);
 
 
 
+
+// which pin to use for reading the sensor? can use any pin!
+#define FLOWSENSORPIN 3
+
+// count how many pulses!
+volatile uint16_t pulses = 0;
+// track the state of the pulse pin
+volatile uint8_t lastflowpinstate;
+// you can try to keep time of how long it is between pulses
+volatile uint32_t lastflowratetimer = 0;
+// and use that to calculate a flow rate
+volatile float flowrate;
+// Interrupt is called once a millisecond, looks for any pulses from the sensor!
+SIGNAL(TIMER0_COMPA_vect) {
+  uint8_t x = digitalRead(FLOWSENSORPIN);
+  
+  if (x == lastflowpinstate) {
+    lastflowratetimer++;
+    return; // nothing changed!
+  }
+  
+  if (x == HIGH) {
+    //low to high transition!
+    pulses++;
+  }
+  lastflowpinstate = x;
+  flowrate = 1000.0;
+  flowrate /= lastflowratetimer;  // in hertz
+  lastflowratetimer = 0;
+}
+
+void useInterrupt(boolean v) {
+  if (v) {
+    // Timer0 is already used for millis() - we'll just interrupt somewhere
+    // in the middle and call the "Compare A" function above
+    OCR0A = 0xAF;
+    TIMSK0 |= _BV(OCIE0A);
+  } else {
+    // do not call the interrupt function COMPA anymore
+    TIMSK0 &= ~_BV(OCIE0A);
+  }
+}
 
 
 /* 
@@ -120,7 +163,13 @@ void setup() {
   
   Serial.println("DHTxx test!");
   dht.begin();
-
+  
+  
+  Serial.print("Flow sensor test!");
+   pinMode(FLOWSENSORPIN, INPUT);
+   digitalWrite(FLOWSENSORPIN, HIGH);
+   lastflowpinstate = digitalRead(FLOWSENSORPIN);
+   useInterrupt(true);
 }
 
 
@@ -201,13 +250,49 @@ void loop() {
   Serial.print(h); Serial.print(",");
   // Serial.print(" %\t");
   // Serial.print("Temperature: "); 
-  Serial.println(t);
+  Serial.print(t); Serial.print(",");
   // Serial.print(" *C ");
   // Serial.print(f);
   // Serial.print(" *F\t");
   // Serial.print("Heat index: ");
   // Serial.print(hi);
   // Serial.println(" *F");
+  
+  
+  
+  
+  // lcd.setCursor(0, 0);
+  // lcd.print("Pulses:"); lcd.print(pulses, DEC);
+  // lcd.print(" Hz:");
+  // lcd.print(flowrate);
+  //lcd.print(flowrate);
+  // Serial.print("Freq: "); 
+  Serial.print(flowrate); Serial.print(",");
+  // Serial.print("Pulses: "); 
+  Serial.print(pulses, DEC); Serial.print(",");
+  
+  // if a plastic sensor use the following calculation
+  // Sensor Frequency (Hz) = 7.5 * Q (Liters/min)
+  // Liters = Q * time elapsed (seconds) / 60 (seconds/minute)
+  // Liters = (Frequency (Pulses/second) / 7.5) * time elapsed (seconds) / 60
+  // Liters = Pulses / (7.5 * 60)
+  float liters = pulses;
+  liters /= 7.5;
+  liters /= 60.0;
+
+/*
+  // if a brass sensor use the following calculation
+  float liters = pulses;
+  liters /= 8.1;
+  liters -= 6;
+  liters /= 60.0;
+*/
+  Serial.println(liters); 
+  // Serial.println(" Liters");
+  // lcd.setCursor(0, 1);
+  // lcd.print(liters); lcd.print(" Liters        ");
+  
+  
   
   delay(5000);
 
