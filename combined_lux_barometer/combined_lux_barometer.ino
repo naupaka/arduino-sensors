c9f12ed0e4ae09f99cc50d1de48a9af180662f54
Naupaka Zimmerman <naupaka@gmail.com>
2014-12-29 14:23:47 -0700

    Combine barometer, light, and time to csv-style output

--- combined_lux_barometer/combined_lux_barometer.ino
+++ combined_lux_barometer/combined_lux_barometer.ino
@@ -1,7 +1,25 @@
+// Output format is YYYYMMDD, 24HRTIME, Barometric pressure in pascals, Calculated altitude in meters, temperature from MPL3115A2 sensor in deg C, light levels in lux  
+
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_TSL2561_U.h>
 #include <Adafruit_MPL3115A2.h>
+#include <Time.h>  
+
+#define TIME_MSG_LEN  11   // time sync to PC is HEADER followed by unix time_t as ten ascii digits
+#define TIME_HEADER  'T'   // Header tag for serial time sync message
+#define TIME_REQUEST  7    // ASCII bell character requests a time sync message 
+
+/* 
+ * TimeSerial.pde
+ * example code illustrating Time library set through serial port messages.
+ *
+ * Messages consist of the letter T followed by ten digit time (as seconds since Jan 1 1970)
+ * you can send the text on the next line using Serial Monitor to set the clock to noon Jan 1 2010
+ T1262347200  
+ *
+ * A Processing example sketch to automatically send the messages is inclided in the download
+ */ 
 
 /* This driver uses the Adafruit unified sensor library (Adafruit_Sensor),
    which provides a common 'type' for sensor data and some helper functions.
@@ -38,6 +56,95 @@
    
 Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
 Adafruit_MPL3115A2 baro = Adafruit_MPL3115A2();
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+  // Get time from processing app on computer
+  setSyncProvider( requestSync);  //set function to call when sync required
+  Serial.println("Waiting for sync message");
+  
+  Serial.println("Light Sensor Test"); Serial.println("");
+  
+  /* Initialise the sensor */
+  if(!tsl.begin())
+  {
+    /* There was a problem detecting the ADXL345 ... check your connections */
+    Serial.print("Ooops, no TSL2561 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+  
+  /* Display some basic information on this sensor */
+  displaySensorDetails();
+  
+  /* Setup the sensor gain and integration time */
+  configureSensor();
+  
+  /* We're ready to go! */
+  Serial.println("");
+  Serial.println("Adafruit_MPL3115A2 test!");
+
+}
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  // to get time
+  if(Serial.available() ) 
+  {
+    processSyncMessage();
+  }
+  if(timeStatus()!= timeNotSet)   
+  {
+    digitalWrite(13,timeStatus() == timeSet); // on if synced, off if needs refresh  
+    digitalClockDisplay(); Serial.print(","); 
+  }
+  // delay(1000);
+  
+  
+  if (! baro.begin()) {
+    Serial.println("Couldnt find sensor");
+    return;
+  }
+  
+  float pascals = baro.getPressure();
+  // Our weather page presents pressure in Inches (Hg)
+  // Use http://www.onlineconversion.com/pressure.htm for other units
+  // Serial.print(pascals/3377); Serial.print(","); // Serial.println(" Inches (Hg)");
+  Serial.print(pascals); Serial.print(","); // Serial.println(" pascals");
+
+  float altm = baro.getAltitude();
+  Serial.print(altm); Serial.print(","); // Serial.println(" meters");
+
+  float tempC = baro.getTemperature();
+  Serial.print(tempC); Serial.print(",");// Serial.println("*C");
+
+  delay(1000);
+  
+    /* Get a new sensor event */ 
+  sensors_event_t event;
+  tsl.getEvent(&event);
+ 
+  /* Display the results (light is measured in lux) */
+  if (event.light)
+  {
+    Serial.print(event.light); Serial.println(); // Serial.println(" lux");
+  }
+  else
+  {
+    /* If event.light = 0 lux the sensor is probably saturated
+       and no reliable data could be generated! */
+    Serial.println("Sensor overload");
+  }
+  delay(5000);
+
+}
+
+
 
 /**************************************************************************/
 /*
@@ -86,66 +193,48 @@ void configureSensor(void)
 }
 
 
-void setup() {
-  // put your setup code here, to run once:
-    Serial.begin(9600);
-  Serial.println("Light Sensor Test"); Serial.println("");
-  
-  /* Initialise the sensor */
-  if(!tsl.begin())
-  {
-    /* There was a problem detecting the ADXL345 ... check your connections */
-    Serial.print("Ooops, no TSL2561 detected ... Check your wiring or I2C ADDR!");
-    while(1);
-  }
-  
-  /* Display some basic information on this sensor */
-  displaySensorDetails();
-  
-  /* Setup the sensor gain and integration time */
-  configureSensor();
-  
-  /* We're ready to go! */
-  Serial.println("");
-  Serial.println("Adafruit_MPL3115A2 test!");
+// time functions
 
+void digitalClockDisplay(){
+  // digital clock display of the time
+  Serial.print(year());
+  Serial.print(month());
+  Serial.print(day());
+  Serial.print(",");
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  // Serial.println(); 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-    if (! baro.begin()) {
-    Serial.println("Couldnt find sensor");
-    return;
+void printDigits(int digits){
+  // utility function for digital clock display: prints preceding colon and leading 0
+  Serial.print(":");
+  if(digits < 10)
+    Serial.print('0');
+  Serial.print(digits);
+}
+
+void processSyncMessage() {
+  // if time sync available from serial port, update time and return true
+  while(Serial.available() >=  TIME_MSG_LEN ){  // time message consists of a header and ten ascii digits
+    char c = Serial.read() ; 
+    Serial.print(c);  
+    if( c == TIME_HEADER ) {       
+      time_t pctime = 0;
+      for(int i=0; i < TIME_MSG_LEN -1; i++){   
+        c = Serial.read();          
+        if( c >= '0' && c <= '9'){   
+          pctime = (10 * pctime) + (c - '0') ; // convert digits to a number    
+        }
+      }   
+      setTime(pctime);   // Sync Arduino clock to the time received on the serial port
+    }  
   }
-  
-  float pascals = baro.getPressure();
-  // Our weather page presents pressure in Inches (Hg)
-  // Use http://www.onlineconversion.com/pressure.htm for other units
-  Serial.print(pascals/3377); Serial.println(" Inches (Hg)");
+}
 
-  float altm = baro.getAltitude();
-  Serial.print(altm); Serial.println(" meters");
-
-  float tempC = baro.getTemperature();
-  Serial.print(tempC); Serial.println("*C");
-
-  delay(1000);
-  
-    /* Get a new sensor event */ 
-  sensors_event_t event;
-  tsl.getEvent(&event);
- 
-  /* Display the results (light is measured in lux) */
-  if (event.light)
-  {
-    Serial.print(event.light); Serial.println(" lux");
-  }
-  else
-  {
-    /* If event.light = 0 lux the sensor is probably saturated
-       and no reliable data could be generated! */
-    Serial.println("Sensor overload");
-  }
-  delay(1000);
-
+time_t requestSync()
+{
+  Serial.write((byte)TIME_REQUEST);  
+  return 0; // the time will be sent later in response to serial mesg
 }
