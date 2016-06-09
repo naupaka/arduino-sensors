7d313175e28ba694fe7549e1518f32a6213892e4
Naupaka Zimmerman <naupaka@gmail.com>
2016-06-09 10:53:56 -0700

    Start adding in RTC and SD code to start and main loops

--- combined_lux_barometer/combined_lux_barometer.ino
+++ combined_lux_barometer/combined_lux_barometer.ino
@@ -152,6 +152,51 @@
 #define TIME_REQUEST  7    // ASCII bell character requests a time sync message 
 
 
+// Set parameters for SD card logging
+//////////////////////////////////////////////////
+// A simple data logger for the Arduino analog pins
+
+// how many milliseconds between grabbing data and logging it. 1000 ms is once a second
+#define LOG_INTERVAL  1000 // mills between entries (reduce to take more/faster data)
+
+// how many milliseconds before writing the logged data permanently to disk
+// set it to the LOG_INTERVAL to write each time (safest)
+// set it to 10*LOG_INTERVAL to write all data every 10 datareads, you could lose up to 
+// the last 10 reads if power is lost but it uses less power and is much faster!
+#define SYNC_INTERVAL 1000 // mills between calls to flush() - to write data to the card
+uint32_t syncTime = 0; // time of last sync()
+
+#define ECHO_TO_SERIAL   1 // echo data to serial port
+#define WAIT_TO_START    0 // Wait for serial input in setup()
+
+// the digital pins that connect to the LEDs
+#define redLEDpin 2
+#define greenLEDpin 3
+
+// The analog pins that connect to the sensors
+#define BANDGAPREF 14            // special indicator that we want to measure the bandgap
+
+#define aref_voltage 3.3         // we tie 3.3V to ARef and measure it with a multimeter!
+#define bandgap_voltage 1.1      // this is not super guaranteed but its not -too- off
+
+RTC_DS1307 RTC; // define the Real Time Clock object
+
+// for the data logging shield, we use digital pin 10 for the SD cs line
+const int chipSelect = 10;
+
+// the logging file
+File logfile;
+
+void error(char *str)
+{
+  Serial.print("error: ");
+  Serial.println(str);
+  
+  // red LED indicates error
+  digitalWrite(redLEDpin, HIGH);
+
+  while(1);
+}
 
 
 // Set parameters for DHT (temperature and humidity sensor) 
@@ -254,6 +299,59 @@ long fsrForce;       // Finally, the resistance converted to force
 
 void setup() {
   Serial.begin(9600);
+  
+    // use debugging LEDs
+  pinMode(redLEDpin, OUTPUT);
+  pinMode(greenLEDpin, OUTPUT);
+  
+   // initialize the SD card
+  Serial.print("Initializing SD card...");
+  // make sure that the default chip select pin is set to
+  // output, even if you don't use it:
+  pinMode(10, OUTPUT);
+  
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    error("Card failed, or not present");
+  }
+  Serial.println("card initialized.");
+  
+  // create a new file
+  char filename[] = "LOGGER00.CSV";
+  for (uint8_t i = 0; i < 100; i++) {
+    filename[6] = i/10 + '0';
+    filename[7] = i%10 + '0';
+    if (! SD.exists(filename)) {
+      // only open a new file if it doesn't exist
+      logfile = SD.open(filename, FILE_WRITE); 
+      break;  // leave the loop!
+    }
+  }
+  
+  if (! logfile) {
+    error("couldnt create file");
+  }
+  
+  Serial.print("Logging to: ");
+  Serial.println(filename);
+
+  // connect to RTC
+  Wire.begin();  
+  if (!RTC.begin()) {
+    logfile.println("RTC failed");
+#if ECHO_TO_SERIAL
+    Serial.println("RTC failed");
+#endif  //ECHO_TO_SERIAL
+  }
+  
+
+  logfile.println("millis,stamp,datetime,light,temp,vcc");    
+#if ECHO_TO_SERIAL
+  Serial.println("millis,stamp,datetime,light,temp,vcc");
+#endif //ECHO_TO_SERIAL
+ 
+  // If you want to set the aref to something other than 5v
+  analogReference(EXTERNAL);
   
   // Get clock time from processing app on computer
   setSyncProvider(requestSync);  //set function to call when sync required
