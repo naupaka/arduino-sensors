2580353494601636e00044b5c5654a9898d1dbf8
Naupaka Zimmerman <naupaka@gmail.com>
2016-06-09 16:18:07 -0700

    Add code to write to SD

--- combined_lux_barometer/combined_lux_barometer.ino
+++ combined_lux_barometer/combined_lux_barometer.ino
@@ -88,7 +88,7 @@
 #include <Time.h>  
 #include <DHT.h>
 #include "RTClib.h"
-
+#include <SD.h>
 
 /* This driver uses the Adafruit unified sensor library (Adafruit_Sensor),
    which provides a common 'type' for sensor data and some helper functions.
@@ -127,6 +127,47 @@
 ///////////////////////////////
 RTC_DS1307 RTC;
 // char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+
+
+// Add code for SD Data logging
+///////////////////////////////
+// how many milliseconds between grabbing data and logging it. 1000 ms is once a second
+#define LOG_INTERVAL  1000 // mills between entries (reduce to take more/faster data)
+
+// how many milliseconds before writing the logged data permanently to disk
+// set it to the LOG_INTERVAL to write each time (safest)
+// set it to 10*LOG_INTERVAL to write all data every 10 datareads, you could lose up to 
+// the last 10 reads if power is lost but it uses less power and is much faster!
+#define SYNC_INTERVAL 1000 // mills between calls to flush() - to write data to the card
+uint32_t syncTime = 0; // time of last sync()
+
+#define ECHO_TO_SERIAL   1 // echo data to serial port
+#define WAIT_TO_START    0 // Wait for serial input in setup()
+
+// the digital pins that connect to the LEDs
+#define redLEDpin 2
+#define greenLEDpin 3
+
+// for the data logging shield, we use digital pin 10 for the SD cs line
+const int chipSelect = 10;
+
+// the logging file
+File logfile;
+
+void error(char *str)
+{
+  Serial.print("error: ");
+  Serial.println(str);
+  
+  // red LED indicates error
+  digitalWrite(redLEDpin, HIGH);
+
+  while(1);
+}
+
+
+
 
 
 // Set parameters for DHT (temperature and humidity sensor) 
@@ -230,6 +271,43 @@ long fsrForce;       // Finally, the resistance converted to force
 void setup() {
   Serial.begin(9600);
   
+    // use debugging LEDs
+  pinMode(redLEDpin, OUTPUT);
+  pinMode(greenLEDpin, OUTPUT);
+
+  // initialize the SD card
+  Serial.print("Initializing SD card...");
+  // make sure that the default chip select pin is set to
+  // output, even if you don't use it:
+  pinMode(10, OUTPUT);
+  
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    error("Card failed, or not present");
+  }
+  Serial.println("card initialized.");
+  
+  // create a new file
+  char filename[] = "LOGGER00.CSV";
+  for (uint8_t i = 0; i < 100; i++) {
+    filename[6] = i/10 + '0';
+    filename[7] = i%10 + '0';
+    if (! SD.exists(filename)) {
+      // only open a new file if it doesn't exist
+      logfile = SD.open(filename, FILE_WRITE); 
+      break;  // leave the loop!
+    }
+  }
+  
+  if (! logfile) {
+    error("couldnt create file");
+  }
+  
+  Serial.print("Logging to: ");
+  Serial.println(filename);
+
+
+  
   // Initialise the TSL2561 light sensor
   if(!tsl.begin()) {
     Serial.print("Ooops, no TSL2561 detected ... Check your wiring or I2C ADDR!");
@@ -272,6 +350,11 @@ void loop() {
 
   // Print time from RTC
   DateTime now = RTC.now();
+    
+  // delay for the amount of time we want between readings
+  // delay((LOG_INTERVAL -1) - (millis() % LOG_INTERVAL));
+  
+  digitalWrite(greenLEDpin, HIGH);  
     
   Serial.print(now.year(), DEC);
   Serial.print('-');
@@ -423,6 +506,21 @@ void loop() {
   }
   // Serial.println("--------------------");
 
+  digitalWrite(greenLEDpin, LOW);
+
+  // Now we write data to disk! Don't sync too often - requires 2048 bytes of I/O to SD card
+  // which uses a bunch of power and takes time
+  // if ((millis() - syncTime) < SYNC_INTERVAL) return;
+  // syncTime = millis();
+  
+  // blink LED to show we are syncing data to the card & updating FAT!
+  digitalWrite(redLEDpin, HIGH);
+  
+  logfile.println("This is a test");
+
+  logfile.flush();
+  digitalWrite(redLEDpin, LOW);
+  
    //////////////////// Pause between sets of measurements  ////////////////////
   // measure once per minute
   delay(60000);
